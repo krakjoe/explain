7ddb6078d98d6ca8569016c4da7f445ad9cd5a16
krakjoe <joe.watkins@live.co.uk>
2013-11-06 21:27:25 +0000

    travis

---
+++ travis/ci.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+wget http://uk3.php.net/get/php-5.4.7.tar.bz2/from/this/mirror -O "php-5.4.7.tar.bz2"
+tar -xf php-5.4.7.tar.bz2
+cd php-5.4.7
+cd ext
+git clone https://github.com/krakjoe/explain.git
+cd ../
+./buildconf --force
+./configure --disable-all 
+make
+TEST_PHP_EXECUTABLE=sapi/cli/php sapi/cli/php run-tests.php ext/explain
